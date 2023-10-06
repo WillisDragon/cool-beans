b254a487de0069aeea34bb07efe606d3bbd98c0e
WillisDragon <bjwillis@ucdavis.edu>
2023-10-05 17:31:26 -0700

    first_py

---
+++ madlib.py
@@ -0,0 +1,15 @@
+'''
+mad libs
+'''
+pluralnoun = input('choose a plural noun')
+place = input('choose a place')
+bodypart = input('choose a body part')
+integer = input('choose a number 0-5')
+adjective = input('choose an adjective')
+adjective2 = input('choose an adjective')
+adjective3 = input('choose an adjective')
+adjective4 = input('choose an adjective')
+adjective5 = input('choose an adjective')
+print('-------------------------------------')
+print('Ian is a', adjective, 'dude, even though I literally know', integer, '% about programming', pluralnoun, ', he let me rotate in his', adjective2, place, '.' 'I am looking forward to both learning the', adjective3, 'language of Python and mastering the', adjective4, 'racing simulator to post my', adjective5, 'lap!')
+print('-------------------------------------')
