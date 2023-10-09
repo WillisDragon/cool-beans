f578ff9eba7ab8d9fd73677796d2b1c4531f1b3a
WillisDragon <bjwillis@ucdavis.edu>
2023-10-09 14:22:53 -0700

    done

--- 21codons.py
+++ 21codons.py
@@ -5,6 +5,7 @@
 # Hint: use the slice operator
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
+print(dna[:3], dna[3:6], dna[6:9], dna[9:12], dna[12:15], dna[15:18], dna[18:21], dna[21:24], dna[24:27])
 
 
 """
