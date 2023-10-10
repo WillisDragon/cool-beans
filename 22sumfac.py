0774531290ac16a6384f0340e158f8c21920d2c4
WillisDragon <bjwillis@ucdavis.edu>
2023-10-10 11:54:21 -0700

    done?

--- 22sumfac.py
+++ 22sumfac.py
@@ -1,3 +1,4 @@
+'''
 # 22sumfac.py
 
 # Write a program that computes the running sum from 1..n
@@ -7,9 +8,22 @@
 # Note: you may not import math or any other library
 
 n = 5 # use this value for your computation
+x = 0
+for i in range(1, n+1):
+	x += i
+	print(x)
+	
+i got 1 3 6 10 15
 
-
+'''
+n = 5
+fac = 1
+for i in range (1, n+1):
+	fac = fac * i
+	print(fac)
 """
+I get 1 2 6 24 120
+
 python3 22sumfac.py
-5 15 120
+5 15 120 (?)
 """
