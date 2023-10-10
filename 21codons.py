2110f28a96fb7ab3e0bdcafc443b4a5fa59698d7
WillisDragon <bjwillis@ucdavis.edu>
2023-10-09 18:13:57 -0700

    done2

--- 21codons.py
+++ 21codons.py
@@ -1,4 +1,4 @@
-# 21codons.py
+"""# 21codons.py
 
 # Print out all the codons for the sequence below in reading frame 1
 
@@ -6,7 +6,10 @@
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 print(dna[:3], dna[3:6], dna[6:9], dna[9:12], dna[12:15], dna[15:18], dna[18:21], dna[21:24], dna[24:27])
-
+"""
+dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
+for i in range(0, len(dna), 3):
+	print(dna[i:i+3])
 
 """
 python3 21codons.py
