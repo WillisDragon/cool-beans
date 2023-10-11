73393e577ff4d46e8e2789b7eb72c98d1cb09008
WillisDragon <bjwillis@ucdavis.edu>
2023-10-10 23:41:16 -0700

    done_clean

--- 24gc.py
+++ 24gc.py
@@ -6,17 +6,17 @@
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
 
-total_G_and_C = 0
+total_GC = 0
 total_nts = 0
 
 for i in range(len(dna)):
 	nt = dna[i]
 	if nt == 'G' or nt == 'C':
-		total_G_and_C += 1
+		total_GC += 1
 	total_nts += 1
 		
 		
-print(f'{total_G_and_C/total_nts:.2f}')	
+print(f'{total_GC/total_nts:.2f}')	
 	
 	
 
