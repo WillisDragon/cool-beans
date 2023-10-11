5662133d1e82eb409b092020327ec477dbba1793
WillisDragon <bjwillis@ucdavis.edu>
2023-10-11 16:38:51 -0700

    done

--- 26anti.py
+++ 26anti.py
@@ -6,9 +6,17 @@
 # Variation: try this with the range() function and slice syntax
 
 dna = 'ACTGAAAAAAAAAAA'
+dnarev = dna[::-1]
+for i in range(len(dnarev)):
+	nt = dnarev[i]
+	if nt == 'A': print('T', end='')
+	elif nt == 'T': print('A', end='')
+	elif nt == 'G': print('C', end='')
+	else:		print('G', end='')
 
 
 """
+
 python3 26anti.py
 TTTTTTTTTTTCAGT
 """
