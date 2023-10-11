9e96c797d9f6ef4a363ba15b0ec7bfa8847ae38a
WillisDragon <bjwillis@ucdavis.edu>
2023-10-10 23:26:47 -0700

    done

--- 24gc.py
+++ 24gc.py
@@ -3,7 +3,25 @@
 # Write a program that computes the GC% of a DNA sequence
 # Format the output for 2 decimal places
 
+
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
+
+total_G_and_C = 0
+total_letters = 0
+
+for i in range(len(dna)):
+	letter = dna[i]
+	if letter == 'G' or letter == 'C':
+		total_G_and_C += 1
+	total_letters += 1
+		
+		
+print(f'{total_G_and_C/total_letters:.2f}')	
+	
+	
+
+
+
 
 
 """
