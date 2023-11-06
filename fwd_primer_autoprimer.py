cd92be98ce6d1d3982439b9833a6782f655ac94d
WillisDragon <bjwillis@ucdavis.edu>
2023-11-06 10:36:12 -0800

    fwdprimerdone

---
+++ fwd_primer_autoprimer.py
@@ -0,0 +1,39 @@
+# fwd_primer_autoprimer
+seq = "ggatggggcacgtggatcgatccatcgatattatattatagcgccgtgatcttttttccagtacttttggctaattgtacg"
+minlen = 20
+maxlen = 27
+minTm = 55
+maxTm = 60
+for i in range(len(seq)-minlen):
+	for j in range(i+minlen, i+maxlen+1):
+		GC_count = 0
+		AT_count = 0
+#make oligo
+		oligo = seq[i:j]
+		for k in range(len(oligo)):
+			nt = oligo[k]
+			length = len(oligo)
+			if nt == 'c' or nt == 'g':
+				GC_count += 1
+			if nt == 'a' or nt == 't':
+				AT_count += 1
+# calc 3' GC
+		threeGC_count = 0
+		threeprime = oligo[length - 5:]
+		for l in range(len(threeprime)):
+			threent = threeprime[l]
+			if threent == 'c' or threent == 'g':
+				threeGC_count += 1
+# find/count substrings
+		SScount = oligo.count("tttttt") + oligo.count("gggg") + oligo.count("aaaaaa") + oligo.count("cccc") + oligo.count("atatata") + oligo.count("agagaga") + oligo.count("acacaca") + oligo.count("tatatat") + oligo.count("tgtgtgt") + oligo.count("tctctct") + oligo.count("gagagag") + oligo.count("gtgtgtg") + oligo.count("gcgcgcg") + oligo.count("cacacac") + oligo.count("ctctctc") + oligo.count("cgcgcgc")
+		GCclamp = oligo.count("gg") + oligo.count("cc") + oligo.count("gc") + oligo.count("cg") - oligo.count("ggg") + oligo.count("ccg") - oligo.count("gcg") - oligo.count("cgg") - oligo.count("ggc") - oligo.count("ccc") - oligo.count("gcc") - oligo.count("cgc") - oligo.count("ggc")
+# calc Tm
+		Tm = 64.9 + 41*(GC_count - 16.4)/(AT_count + GC_count)
+		if Tm >= minTm and Tm <= maxTm:
+			if threeGC_count/5 == 0.6:
+				if SScount == 0:
+					if GCclamp >= 3:
+						if GCclamp <6:
+							print(oligo, "Tm:", f'{Tm:.1f}', "GC:", f'{GC_count/len(oligo):.2f}', "size:", len(oligo))
+
+
